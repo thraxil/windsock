fa1e5ed925a3645620bd6e649857a6cc851f4bd2
Anders Pearson <anders@columbia.edu>
2012-10-18 16:59:24 -0400

    test that invalid tokens fail

--- windsock_test.go
+++ windsock_test.go
@@ -31,3 +31,13 @@ func Test_validateToken(t *testing.T) {
 		t.Error("wrong uni")
 	}
 }
+
+func Test_invalidToken(t *testing.T) {
+	token := "anp8:1344361884:667494:127.0.0.1:306233f64522f1f970fc62fb3cf2d7320c899851GARBAGE"
+	timestamp := time.Unix(1344361884, 0)
+	remote_ip := DummyAddr{"127.0.0.1"}
+	_, err := validateToken(token, timestamp, remote_ip)
+	if err == nil {
+		t.Error("invalid token. should've been an error here")
+	}
+}
