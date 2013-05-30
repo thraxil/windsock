dd000db80dd22d857716f88fbb4861abd61641f8
Anders Pearson <anders@columbia.edu>
2013-05-30 14:13:06 -0400

    json config for broker

--- broker.go
+++ broker.go
@@ -5,26 +5,43 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	zmq "github.com/alecthomas/gozmq"
+	"io/ioutil"
 )
-
-var REP_SOCKET = "tcp://*:5555"
-var PUB_SOCKET = "tcp://*:5556"
 
 type envelope struct {
 	Address string `json:"address"`
 	Content string `json:"content"`
 }
 
+type ConfigData struct {
+	RepSocket string
+	PubSocket string
+}
+
 func main() {
+	var configfile string
+	flag.StringVar(&configfile, "config", "./broker.json", "Broker JSON config file")
+	flag.Parse()
+
+	file, err := ioutil.ReadFile(configfile)
+	if err != nil {
+		fmt.Println("could not read config file")
+	}
+
+	f := ConfigData{}
+	err = json.Unmarshal(file, &f)
+
 	context, _ := zmq.NewContext()
 	pubsocket, _ := context.NewSocket(zmq.PUB)
 	repsocket, _ := context.NewSocket(zmq.REP)
 	defer context.Close()
 	defer pubsocket.Close()
 	defer repsocket.Close()
-	pubsocket.Bind(PUB_SOCKET)
-	repsocket.Bind(REP_SOCKET)
+	pubsocket.Bind(f.PubSocket)
+	repsocket.Bind(f.RepSocket)
 
 	var e envelope
 	for {
